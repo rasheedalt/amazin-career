d1f204e322c06f879f37d76af983468234d96f64
Rasheed <rasheed@initshq.com>
2021-04-05 10:30:53 +0100

    edit 404 error message

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -67,5 +67,5 @@
     <div class="err">4</div>
     <i class="far fa-question-circle fa-spin"></i>
     <div class="err2">4</div>
-    <div class="msg">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?<p>Let's go <a href="/">home</a> and try from there.</p></div>
+    <div class="msg">This page cannot be found. It probably got moved, deleted, hiding out in quarantine or never existed in the first place.<p> Let's go <a href="/">home</a> and try from there.</p></div>
     </div>
