81099d561cfa34eddad8d9e4ba98c574aa076bc2
Rasheed <rasheed@initshq.com>
2021-04-08 09:47:55 +0100

    added copyright year to current year

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -169,7 +169,7 @@
 
   <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2020 Copyright <a class="text-white" href="/">Amazin Careers</a>
+    © {{ date('Y') }} Copyright <a class="text-white" href="/">Amazin Careers</a>
   </div>
   <!-- Copyright -->
 </footer>
